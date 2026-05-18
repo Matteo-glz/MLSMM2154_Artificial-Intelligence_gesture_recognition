ceb89c54901b5cfcee767d8de7c464a2f3ad33d9
Matteo-glz <matteogalizia0407@gmail.com>
2026-05-18 15:53:31 +0200

    image update

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ from pipelines import pipeline_bilstm
 PATH_DOMAIN_1 = "/Users/matteogalizia/Documents/GitHub/MLSMM2154_Artificial-Intelligence_gesture_recognition/GestureData/GestureDataDomain1_Mons/Domain1_csv"
 PATH_DOMAIN_4 = "/Users/matteogalizia/Documents/GitHub/MLSMM2154_Artificial-Intelligence_gesture_recognition/GestureData/GestureDataDomain4_Mons"
 
-K_OPTIONS             = [1]#, 3, 5, 7]
-PCA_OPTIONS           = ["no_pca"]#, 2, 3]
-CLUSTER_OPTIONS       = [15]#, 20, 25, 30, 35, 40]#, 45, 50, 55, 60] # for edit-distance method
-COMPRESSION           = [True]#, False] #  for edit-distance method
-N_POINTS_OPTIONS      = [16]#, 32, 64], #128, 256] # for three-centroid method
-TARGET_LENGTH_OPTIONS = [16]#, 32, 64]#, 128]
-N_UNITS_OPTIONS       = [16]#, 32, 64]#, 128]
+K_OPTIONS             = [1, 3, 5, 7]
+PCA_OPTIONS           = ["no_pca", 2, 3]
+CLUSTER_OPTIONS       = [15, 20, 25, 30, 35, 40]#, 45, 50, 55, 60] # for edit-distance method
+COMPRESSION           = [True, False] #  for edit-distance method
+N_POINTS_OPTIONS      = [16, 32, 64], #128, 256] # for three-centroid method
+TARGET_LENGTH_OPTIONS = [16, 32, 64]#, 128]
+N_UNITS_OPTIONS       = [16, 32, 64]#, 128]
 CV_MODES              = ["dependent", "independent"]
 
 
